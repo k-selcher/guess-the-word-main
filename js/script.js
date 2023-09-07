16ebb52e9be8a4d50a78c6c6eb187d96f8bd512b
selcher_k <selcher@juno.com>
2023-09-07 19:13:50 -0400

    Display word and guessed Letters

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ const validateInput = function(input){
     } else if (input > 1) {
         message.innerText = "Please enter only one letter."
     //If the user enters an input that is not an accepted letter.
-    } else if ((!input.match(acceptedLetter))) {
+    } else if (!input.match(acceptedLetter)) {
         message.innerText = "Please enter only a letter from A to Z."
     } else {
         //Returns if A-Z input
@@ -55,12 +55,47 @@ const validateInput = function(input){
     }
 };
 
-const makeGuess = function(guess){
-    guess = guess.toUpperCase;
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)) {
-        message.innerText = "You already guessed that letter."
-} else {
-    guessedLetters.push(guess);
-    console.log(guessedLetters);
+      message.innerText = "You already guessed that letter.";
+    } else {
+      guessedLetters.push(guess);
+      console.log(guessedLetters);
+      showGuessedLetters();
+      updateWordInProgress(guessedLetters);
     }
+};
+
+const showGuessedLetters = function () {
+    //Clears the list
+    guessedLettersElement.innerText = "";
+    for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li)
+    }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+  // console.log(revealWord);
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
 };
